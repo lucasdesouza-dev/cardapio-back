dde04e94e0b2db1d95526f8e40e0eb2723d6f466
Lucas Henrique de Souza <55006206+lucasdesouza-dev@users.noreply.github.com>
2023-11-20 14:44:28 -0300

    Initial commit

---
+++ src/services/Methods/Methods.service.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class MethodsService {
+  constructor(private readonly prisma: PrismaService) { }
+  async checkUser(userTenant, id, tabela) {
+    const item = await this.prisma[tabela].findUnique({
+      where: { uuid: id },
+    });
+    const tenantUuidItem = item.tenantUuid;
+    const aprovado = userTenant === tenantUuidItem;
+
+    return aprovado;
+  }
+}
